49aae977de6a66c2d553e99bb2c4ef9bb33ae95c
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-04-24 14:23:58 +0800

    Finished retrieving data feature and auto generating table with the required columns and values. Only left with tidying up the UI to make it look nicer and formatting the required columns

---
+++ README.md
@@ -0,0 +1,32 @@
+# AUTOMATIC AIRBNB COMPARER
+
+## Video Demo
+
+`https://youtu.be/I9ZvV8Vgxog`
+
+## Description
+
+### Problem
+
+When browsing airbnb listings and choosing which listing to go for, it can often be
+difficult to choose and compare which listing one wants. After all - they all look pretty
+good, don't they? Inputting all the information into an excel spreadsheet is also often
+troublesome and annoying, and frankly takes maybe way too long. Hence, I created an desktop
+app to try to address the situation.
+
+### Specifications
+
+To add a listing, one simply presses the add a listing option. A new page pops up which
+requires a user to input the link (with the id, check in and check out dates), the (daily)
+cost, and miscellaneous costs (airbnb service charge + cleaning fee). The entry is then
+automatically entered into a SQLite3 database for storage.
+
+To compare, simply go to the main page and input the location that one wants to compare the
+listings for. Then, using the checkboxes, select the columns that one wants to show. After pressing
+submit, the app automatically retrieves the required listings and displays the columns selected
+in a dynamically generated table.
+
+### Languages used
+
+Python
+SQLite3
--- project.py
+++ project.py
@@ -1,10 +1,14 @@
-import gobnb, json, tkinter, customtkinter, sqlite3, re
+import gobnb, json, tkinter, customtkinter, sqlite3, re, textwrap, requests
 from datetime import datetime
+from tkinter import ttk, PhotoImage
+from PIL import Image, ImageTk
+from io import BytesIO
+
+
+DATABASE = r"db\database.db"
 
 
 class Listing:
-
-    DATABASE = r"CS50x\db\database.db"
 
     @classmethod
     def add_listing(
@@ -45,13 +49,16 @@ class Listing:
         info["coordinates"] = json.dumps(data["coordinates"])
         info["super_host"] = data["is_super_host"]
         info["capacity"] = data["person_capacity"]
-        
 
         # Average rating
-        info["average_rating"] = (
-            sum(float(value) for key, value in data["rating"].items())
-            - float(data["rating"]["review_count"])
-        ) / (len(data["rating"]) - 1)
+        info["average_rating"] = round(
+            (
+                sum(float(value) for key, value in data["rating"].items())
+                - float(data["rating"]["review_count"])
+            )
+            / (len(data["rating"]) - 1),
+            2,
+        )
 
         # Check in and out
         info["check_in_out"] = [
@@ -83,16 +90,16 @@ class Listing:
             if dict_pair["title"].lower() == "getting around":
                 info["getting_around"] = dict_pair["content"].replace("<br />", "\n")
             else:
-                info["location"] = dict_pair["title"]
+                info["location"] = dict_pair["title"].replace("\u014d", "o")
 
         print("Scraping completed, all filtering of data done")
         status.configure(text="Scraping completed, all filtering done")
 
         # Creates the tables to store the description if they do not yet exist
-        create_tables(cls.DATABASE)
+        create_tables(DATABASE)
 
         # Add the entry into SQLite
-        sql_add_entry(cls.DATABASE, info, root)
+        sql_add_entry(DATABASE, info, status)
 
     @classmethod
     def extract_from_url(cls, link: str) -> None:
@@ -135,7 +142,7 @@ class Listing:
             dict: key value pairs of the required information
         """
 
-        conn = sql_create_connection(cls.database)
+        conn = sql_create_connection(DATABASE)
 
         with conn:
             cursor = conn.cursor()
@@ -161,7 +168,7 @@ class Listing:
 
 
 def main():
-    
+
     # Setting up the title page
     root = customtkinter.CTk()
     root_set_up(root)
@@ -181,70 +188,147 @@ def root_set_up(root: customtkinter.CTk) -> None:
     # Getting dimensions of screen
     width = 0.5 * root.winfo_screenwidth()
     height = 0.5 * root.winfo_screenheight()
-    
-    # Setting pop up to be full size              
+
+    # Setting pop up to be full size
     root.geometry("%dx%d" % (width, height))
     root.title("Airbnb")
-    
+
     # Setting up UI for main page
-    
+
     # Define a grid for the compare screen
-    root.columnconfigure(0, weight=1)
-    root.columnconfigure(1, weight=8)
-    root.rowconfigure(0, weight=12)
+    root.columnconfigure(0, weight=1, uniform="a")
+    root.columnconfigure(1, weight=3, uniform="a")
+    root.rowconfigure(0, weight=4)
+    root.rowconfigure(1, weight=8)
     root.rowconfigure(1, weight=1)
     root.rowconfigure(1, weight=1)
-    
+
     # Place button in bottom left
-    button_insert = customtkinter.CTkButton(root, text="Insert a Listing!", command=lambda: new_insert(root, status))
+    button_insert = customtkinter.CTkButton(
+        root, text="Insert a Listing!", command=lambda: new_insert(root, status)
+    )
     button_insert.grid(row=1, column=0, sticky="s")
-    
+
     # Status Label
     status = customtkinter.CTkLabel(root, text="This is where status will go")
-    status.grid(row=2, column=0, sticky="n",  pady=10)
-    
+    status.grid(row=2, column=0, sticky="n", pady=10)
+
     # Place tabview menu at top left
     menu_tab = customtkinter.CTkTabview(root)
     menu_tab.grid(row=0, column=0, sticky="nsew", padx=20, pady=3)
-    
+
     # Create tabview
     tab_main = menu_tab.add("Menu")
     tab_settings = menu_tab.add("Settings")
-    
+
     # Setting up main in tabview
     tab_main_title = customtkinter.CTkLabel(tab_main, text="Hello")
     tab_main_title.pack()
-    
+
     # TODO: Insert main title page (check which columns to show, which location to search, and title) and settings (which columns to show, which amenities to show)
-    
+
+    # Search by location
+    search_tag = customtkinter.CTkLabel(tab_main, text="Location")
+    search_tag.pack(padx=20, pady=30)
+
+    location = tkinter.StringVar()
+    search_location = customtkinter.CTkEntry(
+        tab_main,
+        placeholder_text="Location (One word)",
+        height=40,
+        textvariable=location,
+    )
+    search_location.pack(pady=5, padx=20)
+
+    checkbox_title = customtkinter.CTkLabel(
+        tab_main, padx=20, pady=20, text="Show Columns"
+    )
+    checkbox_title.pack(padx=20)
+
+    checkbox_frame = customtkinter.CTkFrame(tab_main)
+    checkbox_frame.pack(fill="both", expand=True, pady=0)
+
+    columns_selected = create_checkboxes(checkbox_frame)
+
+    search_button = customtkinter.CTkButton(
+        tab_main,
+        text="Search!",
+        command=lambda: create_table(search_location.get(), table, columns_selected),
+    )
+    search_button.pack(pady=10, padx=20)
+
     # Place scrollable frame for table
-    table_frame = customtkinter.CTkScrollableFrame(root, orientation="vertical")
+    table_frame = customtkinter.CTkFrame(root)
     table_frame.grid(row=0, column=1, stick="nsew", padx=20, pady=20, rowspan=3)
+
+    # Places a auto_setup table
+
+    def delete_items(_):
+        for i in table.selection():
+            table.delete(i)
+
+    # Setting up the style of the table
+    style = ttk.Style()
+    font_size = 18
+
+    style.theme_use("default")
+    style.configure(
+        "Treeview",
+        background="#2a2d2e",
+        foreground="white",
+        rowheight=120,
+        fieldbackground="#343638",
+        bordercolor="#343638",
+        borderwidth=0,
+        font=(None, font_size),
+        padx=10,
+        pady=10,
+    )
+    style.map("Treeview", background=[("selected", "#22559b")])
+
+    style.configure(
+        "Treeview.Heading",
+        background="#565b5e",
+        foreground="white",
+        relief="flat",
+        font=(None, font_size),
+    )
+    style.map("Treeview.Heading", background=[("active", "#3484F0")])
     
-    # TODO: Add a auto generated table
     
+    table_y_scroll = ttk.Scrollbar(table_frame)
+    table_y_scroll.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    
+    table_x_scroll = ttk.Scrollbar(table_frame, orient="horizontal")
+    table_x_scroll.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+
+    table = tkinter.ttk.Treeview(table_frame, show=("headings", "tree"), yscrollcommand=table_y_scroll.set, xscrollcommand=table_x_scroll)
+    table.bind("<Delete>", delete_items)
+    
+    table_y_scroll.config(command=table.yview)
+    table_x_scroll.config(command=table.xview)
+
 
 def new_insert(root: customtkinter.CTk, status: customtkinter.CTkLabel) -> None:
     """Creates a new window for inserting new listings"""
     insert = customtkinter.CTkToplevel(root)
     insert.attributes("-topmost", True)
-    
+
     # insert Frame
     insert.title("Airbnb | add link")
-    
+
     # Setting up grid
-    insert.columnconfigure((1,2,3,4), weight=1)
+    insert.columnconfigure((1, 2, 3, 4), weight=1)
     insert.rowconfigure(1, weight=3)
     insert.rowconfigure(3, weight=1)
 
     title = customtkinter.CTkLabel(insert, text="Add a New Listing!")
     title.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
-    
 
     # Link Input
     link_label = customtkinter.CTkLabel(insert, text="Link:")
     link_label.grid(row=1, column=0, sticky="e", padx=20)
-    
+
     link_var = tkinter.StringVar()
     link = customtkinter.CTkEntry(
         insert, placeholder_text="Link", height=40, textvariable=link_var
@@ -254,18 +338,22 @@ def new_insert(root: customtkinter.CTk, status: customtkinter.CTkLabel) -> None:
     # Daily cost input
     cost_label = customtkinter.CTkLabel(insert, text="Cost:")
     cost_label.grid(row=2, column=0, pady=10, sticky="e", padx=20)
-    
-    cost_var = tkinter.IntVar()
+
+    cost_var = tkinter.StringVar()
     cost = customtkinter.CTkEntry(
-        insert, placeholder_text="Daily Cost", width=100, height=40, textvariable=cost_var
+        insert,
+        placeholder_text="Daily Cost",
+        width=100,
+        height=40,
+        textvariable=cost_var,
     )
     cost.grid(row=2, column=1, padx=20, pady=10, sticky="w")
 
     # Miscellaneous cost input
     misc_cost_label = customtkinter.CTkLabel(insert, text="Misc Cost:")
     misc_cost_label.grid(row=2, column=2, pady=10, sticky="e")
-    
-    misc_cost_var = tkinter.IntVar()
+
+    misc_cost_var = tkinter.StringVar()
     misc_cost = customtkinter.CTkEntry(
         insert,
         placeholder_text="Miscellaneous Cost",
@@ -281,32 +369,153 @@ def new_insert(root: customtkinter.CTk, status: customtkinter.CTkLabel) -> None:
         text="Input",
         command=lambda: (
             Listing.add_listing(
-                link.get().strip(),
-                [cost.get(), misc_cost.get()],
-                status
-            ),
-            input_clear(link, cost, misc_cost)
-        )
+                link.get().strip(), [cost.get(), misc_cost.get()], status
+            )
+        ),
     )
-    button_input.grid(row=3, column=1, pady=20, padx=20, sticky="e", columnspan=2)
-    
-    
+    button_input.grid(row=3, column=1, pady=20, padx=(50, 0), columnspan=2, sticky="e")
 
 
-# BUG Not clearing field
-def input_clear(link: customtkinter.CTkEntry, cost: customtkinter.CTkEntry, misc_cost: customtkinter.CTkEntry) -> None:
+def create_table(
+    location: str, table: tkinter.ttk.Treeview, list_all_columns: dict
+) -> None:
     """
-    Clears the input field in the main box
+    Dynamically generates a table in the main screen
 
     Args:
-        link (customtkinter.CTkEntry): link field
-        cost (customtkinter.CTkEntry): cost field
-        misc_cost (customtkinter.CTkEntry): misc cost field
+        location (str): location that the user has inputted
+        table (tkinter.ttk.Treeview): the tkinter module for the table
+        list_all_columns (dict): the list of all the columns that the user has selected
     """
-    link.delete(0, customtkinter.END)
-    cost.delete(0, customtkinter.END)
-    misc_cost.delete(0, customtkinter.END)
+
+    table.delete(*table.get_children())
+
+    def wrap(string, length=120):
+        return "\n".join(textwrap.wrap(string, width=length))
+
+    # To get the list of columns that the user has selected
+    list_col = ["Cover"]
+    for column, value in list_all_columns.items():
+        if value.get() == True:
+            list_col.append(column)
+
+    table.config(columns=list_col)
     
+    for item in list_col:
+        table.heading(item, text=item, anchor="center")
+        table.column(item, minwidth=25, width=300, stretch=tkinter.NO)
+
+    # Get the list of listings from the location
+    listings = retrieve_from_location(DATABASE, location)
+
+    # To hold list of images
+    image_list = []
+    for listing in listings:
+        row = ["",]
+        for col in list_col[1::]:
+            row.append(wrap(str(retrieve_from_json(col, listing)), 14))
+
+        image_link = retrieve_from_json("Cover", listing)
+        response = requests.get(image_link)
+        
+        # Formats image
+        image = Image.open(BytesIO(response.content))
+        
+        new_height = 120
+        new_width = int(new_height / image.height * image.width)
+        image = image.resize((new_width, new_height))
+        image = ImageTk.PhotoImage(image)
+        
+        image_list.append(image)
+        
+        table.insert("", 0, image=image, values=row)
+        
+    table.image = image_list
+    print("Table should be created")
+
+    table.pack(expand=True, fill="both", anchor="w")
+
+
+def retrieve_from_json(column: str, listing: dict) -> any:
+    """
+    Retrieves the required data from the given dictionary of listing
+
+    Args:
+        column (str): The column that the data belongs to
+        listing (dict): The json/ dict of the listing
+
+    Returns:
+        any: returns the entry, which can be in any form such as a string or list
+    """
+
+    checkbox_contents = {
+        "ID": "id",
+        "Rating": "average_rating",
+        "URL": "url",
+        "Duration": "duration",
+        "Coordinates": "coordinates",
+        "Getting Around": "getting_around",
+        "Check In/ Out Timing": "check_in_out",
+        "Layout": "layout",
+        "Capacity": "capacity",
+        "Cost": "cost",
+        "Super Host": "super_host",
+        "Amenities": "amenities",
+        "Notes": "notes",
+        "Images": "images",
+        "Cover": "cover",
+    }
+
+    column = checkbox_contents[column]
+
+    if column == "cost":
+        # Calculate total cost
+        return int(listing["daily_cost"]) * listing["duration"] + int(
+            listing["misc_cost"]
+        )
+
+    elif column == "cover":
+        # Get the cover photo
+        return listing["images"][2]
+
+    else:
+        return listing[column]
+
+
+def create_checkboxes(frame: customtkinter.CTkFrame) -> dict:
+    """Sets up the checkboxes"""
+    checkbox_variables = {}
+
+    def create_checkbox(text: str, row: int, column: int) -> dict:
+        """Create a check box in the given row and column"""
+        var = customtkinter.BooleanVar()
+        checkbox = customtkinter.CTkCheckBox(frame, text=text, variable=var)
+        checkbox.grid(row=row, column=column, sticky="w", padx=10, pady=5)
+        # Adds the checkbox variable to the dictionary
+        checkbox_variables[text] = var
+
+    checkbox_contents = {
+        "ID": "id",
+        "Rating": "average_rating",
+        "URL": "url",
+        "Duration": "duration",
+        "Coordinates": "coordinates",
+        "Getting Around": "getting_around",
+        "Check In/ Out Timing": "check_in_out",
+        "Layout": "layout",
+        "Capacity": "capacity",
+        "Cost": "cost",
+        "Super Host": "super_host",
+        "Amenities": "amenities",
+        "Notes": "notes",
+        "Images": "images",
+    }
+
+    for i, text in enumerate(checkbox_contents):
+        create_checkbox(text, i // 2, i % 2)
+
+    return checkbox_variables
+
 
 def sql_create_connection(db_file: str) -> sqlite3.Connection:
     """
@@ -357,7 +566,7 @@ def create_tables(database: str) -> None:
                                 ); """
 
     sql_create_basic_info_table = """ CREATE TABLE IF NOT EXISTS basic_info (
-                                        listing_id integer NOT NULL,
+                                        listing_id integer UNIQUE NOT NULL,
                                         coordinates,
                                         location,
                                         getting_around,
@@ -372,7 +581,7 @@ def create_tables(database: str) -> None:
                                     ); """
 
     sql_create_others_table = """ CREATE TABLE IF NOT EXISTS others (
-                                        listing_id integer NOT NULL,
+                                        listing_id integer UNIQUE NOT NULL,
                                         amenities,
                                         images,
                                         notes,
@@ -380,7 +589,7 @@ def create_tables(database: str) -> None:
                                     ); """
 
     sql_create_json_table = """ CREATE TABLE IF NOT EXISTS json (
-                                        listing_id integer NOT NULL,
+                                        listing_id integer UNIQUE NOT NULL,
                                         json,
                                         FOREIGN KEY (listing_id) REFERENCES main (id)
                                     ); """
